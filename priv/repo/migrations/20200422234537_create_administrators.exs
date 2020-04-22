53f8dc4e0981512a0cc8b6dab1be25147cbb8c03
gissandrogama <gissandrogama@gmail.com>
2020-04-22 20:49:35 -0300

    create schema and migrations

---
+++ priv/repo/migrations/20200422234537_create_administrators.exs
@@ -0,0 +1,14 @@
+defmodule Claim.Repo.Migrations.CreateAdministrators do
+  use Ecto.Migration
+
+  def change do
+    create table(:administrators) do
+      add :name, :string
+      add :email, :string
+      add :password_hash, :string
+
+      timestamps()
+    end
+
+  end
+end
