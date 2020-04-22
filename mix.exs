4d7c080d3483dc68b8a46b93b6f49f51025f0e6c
gissandrogama <gissandrogama@gmail.com>
2020-04-22 18:49:09 -0300

    install credo

--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule Claim.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:credo, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
 
